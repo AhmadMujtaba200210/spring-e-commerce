0189c64edc44483a2022c51464ecf881ccc5ec04
AhmadMujtaba <mujtaba200210@gmail.com>
2023-07-25 20:52:47 +0500

    feat: Project structure has completed Successfully

---
+++ e-commerce/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/admin/MainController.java
@@ -0,0 +1,13 @@
+package com.shopme.shopmebackend.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String viewHomePage(){
+        return "indexWelcome";
+    }
+
+}
