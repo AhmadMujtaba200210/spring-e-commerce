43454d28b38b8f64606f142937e714b194de7a79
AhmadMujtaba <mujtaba200210@gmail.com>
2023-08-01 18:59:48 +0500

    refactor: remove extra packaging

--- e-commerce/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ e-commerce/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -1,9 +1,11 @@
-package com.shopme.shopmebackend.admin;
+package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.shopme.shopmecommon.entity","com.shopme.admin.user"})
 public class ShopmeBackEndApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopmeBackEndApplication.class);
