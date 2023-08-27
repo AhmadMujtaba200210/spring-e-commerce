81fd0ba04e0f3d6f5cb2cbf6d399a8d3baf59c14
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-08-27 15:06:40 +0500

    style: displaying roles of users

--- e-commerce/ShopmeCommon/src/main/java/com/shopme/shopmecommon/entity/Role.java
+++ e-commerce/ShopmeCommon/src/main/java/com/shopme/shopmecommon/entity/Role.java
@@ -56,8 +56,6 @@ public class Role {
 
     @Override
     public String toString() {
-        return "Role{" +
-               "name='" + name + '\'' +
-               '}';
+        return this.name;
     }
 }
