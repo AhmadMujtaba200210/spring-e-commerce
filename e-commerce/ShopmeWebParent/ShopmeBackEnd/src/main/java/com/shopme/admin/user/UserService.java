442bc1dd126ea051b09db889299679027d461652
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-08-31 19:54:49 +0500

    feat: listing user roles

--- e-commerce/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ e-commerce/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import com.shopme.shopmecommon.entity.Role;
 import com.shopme.shopmecommon.entity.User;
 import org.springframework.stereotype.Service;
 
@@ -8,12 +9,20 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository repo;
-
-    public UserService(UserRepository repo) {
+    private final RoleRepository roleRepository;
+    public UserService(UserRepository repo, RoleRepository roleRepository) {
         this.repo = repo;
+        this.roleRepository = roleRepository;
     }
 
     public List<User> listAll(){
         return  (List<User>)  repo.findAll();
+    }
+    public List<Role> listRoles(){
+        return  (List<Role>)  roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        repo.save(user);
     }
 }
