acf84afb9298d1609912fec3ceb30fd14bd61a0f
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-08-31 19:54:04 +0500

    feat: user creation api

--- e-commerce/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ e-commerce/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,9 +1,12 @@
 package com.shopme.admin.user;
 
+import com.shopme.shopmecommon.entity.Role;
 import com.shopme.shopmecommon.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,7 +26,22 @@ public class UserController {
     }
 
     @GetMapping("/users/new")
-    public String newUser(){
+    public String newUser(Model model){
+        List<Role> listRoles=service.listRoles();
+        User user=new User();
+        user.setEnabled(true);
+        model.addAttribute("user",user);
+        model.addAttribute("listRoles",listRoles);
+
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes){
+        System.out.println(user);
+        service.save(user);
+
+        redirectAttributes.addFlashAttribute("message","The user has been saved successfully");
+        return "redirect:/users";
     }
 }
